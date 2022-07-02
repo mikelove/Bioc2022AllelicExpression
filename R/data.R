3a8ca06f7370e314384f75462acb6766b4323c87
mikelove <michaelisaiahlove@gmail.com>
2022-07-02 10:31:38 -0700

    try again

---
+++ R/data.R
@@ -0,0 +1,34 @@
+#' Osteoblast gene-level data for chr1
+#'
+#' Subset of the osteoblast dataset, summarized to gene level.
+#'
+#' @references
+#'
+#' The osteoblast differentiation experiment is described here:
+#'
+#' \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE54461}
+#'
+#' The quantification steps are provided here:
+#' 
+#' \url{https://github.com/mikelove/osteoblast-quant}
+#' @format  SummarizedExperiment
+#' @usage data(osteoblast_gene_chr1)
+"osteoblast_gene_chr1"
+
+#' Osteoblast tss-level data for chr1
+#'
+#' Subset of the osteoblast dataset, summarized to TSS level
+#' (50 bp resolution).
+#'
+#' @references
+#'
+#' The osteoblast differentiation experiment is described here:
+#'
+#' \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE54461}
+#'
+#' The quantification steps are provided here:
+#' 
+#' \url{https://github.com/mikelove/osteoblast-quant}
+#' @format  SummarizedExperiment
+#' @usage data(osteoblast_tss_chr1)
+"osteoblast_tss_chr1"
